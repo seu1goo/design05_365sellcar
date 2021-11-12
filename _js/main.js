89aaf1459b705990ae3ebe967960629e8b243274
seu1goo <seu1goo@naver.com>
2021-11-12 12:30:04 +0900

    sp dev

--- _js/main.js
+++ _js/main.js
@@ -1,54 +1,56 @@
-const header = document.querySelector('header')
-const logo = document.querySelector('.logo img')
-window.addEventListener('scroll', _.throttle(function () {
-  if (window.scrollY < 100) {
-    header.classList.remove('move-down')
-    logo.setAttribute('src', './_img/logo02_w.png')
-
-  } else {
-    header.classList.add('move-down')
-    logo.setAttribute('src', './_img/logo02.png')
-  }
-}, 300));
+const header = document.querySelector("header");
+const logo = document.querySelector(".logo img");
+window.addEventListener(
+  "scroll",
+  _.throttle(function () {
+    if (window.scrollY < 100) {
+      header.classList.remove("move-down");
+      logo.setAttribute("src", "./_img/logo02_w.png");
+    } else {
+      header.classList.add("move-down");
+      logo.setAttribute("src", "./_img/logo02.png");
+    }
+  }, 300)
+);
 // header
 
-new Swiper('.mainVisual .swiper-container', {
+new Swiper(".mainVisual .swiper-container", {
   pagination: {
-    el: '.swiper-pagination',
-    clickable: true
+    el: ".swiper-pagination",
+    clickable: true,
   },
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
   autoplay: {
     delay: 7000,
     disableOnInteraction: false,
   },
-  loop: true
+  loop: true,
 });
 // mainCon
 
-$('.counter').counterUp({
+$(".counter").counterUp({
   delay: 10,
-  time: 1000
+  time: 1000,
 });
 
-$('.roofSell').vTicker('init', {
+$(".roofSell").vTicker("init", {
   speed: 1000,
   pause: 1000,
   showItems: 10,
   height: 210,
-  margin: 5
+  margin: 5,
 });
 // counseling
 
-new Swiper('#review .swiper-container', {
-  slidesPerView: 1,
+new Swiper("#review .swiper-container", {
+  slidesPerView: 2.01,
   spaceBetween: 20,
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
   autoplay: {
     delay: 7000,
@@ -61,7 +63,7 @@ new Swiper('#review .swiper-container', {
     },
     640: {
       slidesPerView: 1,
-    }
-  }
+    },
+  },
 });
 // review
